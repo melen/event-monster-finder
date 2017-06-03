ff39a6a5a7a1a33f863c90446dce31dff0ca4f48
Haowei <hweigao@hotmail.com>
2017-06-03 14:40:05 -0400

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+module.exports = function FindEventMobs(dispatch) {
+    let long = require("long");
+    let userId;
+    let translate = require('./lib/translate/lib/translate');
+
+    dispatch.hook('S_LOGIN', 1, event => {
+        userId = event.playerId;
+        console.log("Login");
+    });
+
+    dispatch.hook('S_SPAWN_NPC', 3, event => {
+        if (event.npcName == '이벤트 보물상자') {
+            console.log(spawnId);
+            dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
+                unk1: 42,
+                unk2: 0,
+                unk3: 27,
+                message: "Blue Box Found"
+            });
+
+            itemID = event.id.toInt();
+            dispatch.toClient('S_SPAWN_DROPITEM', 1, {
+                id: itemID,
+                x: event.x,
+                y: event.y,
+                z: event.z,
+                item: 98261,
+                amount: 1,
+                expiry: 999999,
+                owners: [{id: userId}]
+            });
+        }
+    });
+
+    dispatch.hook('S_DESPAWN_NPC', 1, event => {
+        spawnId = event.target.toString();
+        itemID = event.target.toInt();
+        if (spawnId > '4925812090000000') {
+            dispatch.toClient('S_DESPAWN_DROPITEM', 1, {
+                id: itemID
+            });
+        }
+    });
+};
